776f1f4ccba585b46edfd8dbe696f6b9b4c80ce6
Vijay Srinivasan <cbevijay@gmail.com>
2020-05-09 08:36:16 +0530

    1. Change resolution of dice image vectors to 200 * 200 2. Updated app to use ImageView instead of TextView

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -16,22 +17,39 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {rollDice()}
 
+        /*
         val countUpButton: Button = findViewById(R.id.count_button)
         countUpButton.setOnClickListener {incrementCount()}
 
         val resetButton: Button = findViewById(R.id.reset_button)
         resetButton.setOnClickListener {resetCount()}
+         */
     }
 
     private fun rollDice() {
         val randomInt = (1..6).random()
+        /*
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
 
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
+         */
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 
+    /*
     private fun incrementCount() {
         val resultText: TextView = findViewById(R.id.result_text)
 
@@ -51,5 +69,6 @@ class MainActivity : AppCompatActivity() {
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = "0"
     }
+    */
 
 }
